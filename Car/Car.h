37432261ac30cd79d65d53f079ef26df33210349
tanadendere <74002607+tanadendere@users.noreply.github.com>
2020-11-07 13:08:06 +0200

    Update Car.h

--- Car/Car.h
+++ Car/Car.h
@@ -1,1 +1,54 @@
+#ifndef CAR_H
+#define CAR_H
+#include "CarPart.h"
+#include <iostream>
+using namespace std;
 
+
+class Car {
+
+private:
+	CarPart** parts;
+	string name;
+	string driver;
+	string team;
+	string testdriver;
+	int position;
+	long qualifyingtime;
+
+public:
+	Car();
+	
+	Car(string n, string d, string td, string t);
+
+	~Car();
+
+	void descrption();
+
+	void add(CarPart* p);
+
+	void remove(string id);
+
+	string getName();
+
+	void setName(string name);
+	
+	string getDriver();
+	
+	string getTestDriver();
+	
+	string getTeam();
+	
+	int getPosition();
+	
+	void setPosition();
+	
+	long getTime();
+	
+	void setTime();
+	
+	
+	
+};
+
+#endif
