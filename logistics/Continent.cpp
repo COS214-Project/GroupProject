df4f187c25f3d2b471a511d4153025903118a986
Zama0405 <73443014+Zama0405@users.noreply.github.com>
2020-11-07 23:44:25 +0200

    Update Continent.cpp

--- logistics/Continent.cpp
+++ logistics/Continent.cpp
@@ -10,6 +10,7 @@ Continent::~Continent() {
 
 void Continent::travel() {
 	cout<<"The cars are being flown to the meat"<<endl;
+	getCar()->description();
 }
 
 
@@ -21,10 +22,10 @@ void Continent::setCarry(Container* carry) {
 	this->carry = carry;
 }
 
-// Car* Continent::getCar() {
-// 	return this->car;
-// }
+Car* Continent::getCar() {
+	return this->car;
+}
 
-// void Continent::setCar(Car* car) {
-// 	this->car = car;
-// }
+void Continent::setCar(Car* car) {
+	this->car = car;
+}
