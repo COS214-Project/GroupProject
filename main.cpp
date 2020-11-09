fe3df111888fdeaf75dea89d85551becab35f786
khotsoB <73998710+khotsoB@users.noreply.github.com>
2020-11-10 00:21:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,94 @@
 
+#include "Car.h"
+
+#include "NewAerodynamics.h"
+#include "NewElectronics.h"
+#include "NewEngine.h"
+#include "NewChassis.h"
+
+#include "CurrentAerodynamics.h"
+#include "CurrentElectronics.h"
+#include "CurrentEngine.h"
+#include "CurrentChassis.h"
+
+#include "AerodynamicsFactory.h"
+#include "ElectronicsFactory.h"
+#include "EngineFactory.h"
+#include "ChassisFactory.h"
+
+
+int main(){
+  
+//     forllow order
+//       Engineering
+//       Testing
+//       Simulation
+//       Racing Start
+//       Logistics
+//       Race
+      
+      
+  
+  
+    cout<<"=============Engineering============\n";
+    cout<<"=============Creating New Car For Next Season============\n";
+    AerodynamicsFactory* a = new AerodynamicsFactory();
+    ElectronicsFactory* b = new ElectronicsFactory();
+    EngineFactory* c = new EngineFactory();
+    ChassisFactory* d = new ChassisFactory();
+
+    Car* enzo = new Car("Enzo","","","");
+    
+    enzo->add(a->build(1,"2.5",0,0));
+    enzo->add(b->build(1,"TAG-320B",130,4));
+    enzo->add(c->build(1,"v8",14000,900));
+    enzo->add(d->build(1,"carbon fibre",13,800));
+    
+    
+    cout<<endl;
+    enzo->descrption();
+  
+   cout<<"\n=============Creating Current Car For This Season============\n";
+
+    Car* SF90 = new Car("SF90","","","");
+  
+    CarPart* Ca = new CurrentAerodynamics();
+    CarPart* Cb = new CurrentElectronics();
+    CarPart* Cc = new CurrentEngine();
+    CarPart* Cd = new  CurrentChassis();
+  
+    cout<<"\n=============Creating Current Car Parts============\n";
+    Ca = a->build(0,"2.8",0,0);
+    Cb = b->build(0,"TAG-320",120,4);
+    Cc = c->build(0,"v8",12000,900);
+    Cd = d->build(0,"carbon fibre",12,740);
+  
+    
+    cout<<"\n=============Creating Copies For Current Car Parts And Adding Them To Car============\n";
+    SF90->add(Ca->clone());
+    SF90->add(Cb->clone());
+    SF90->add(Cc->clone());
+    SF90->add(Cd->clone());
+   
+    cout<<endl;
+    SF90->descrption();
+
+    delete a;
+    delete b;
+    delete c;
+    delete d;
+
+    delete Ca;
+    delete Cb;
+    delete Cc;
+    delete Cd;
+
+    delete enzo;
+    delete SF90;
+
+
+
+    
+    return 0;
+}
+
